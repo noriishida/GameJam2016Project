60a77b29004e4b0876de46a286264c43de1046bb
norioishidda <aoiaoitikyuu@gmail.com>
2016-01-30 22:41:51 +0900

    一時的にコミット

---
+++ Assets/Script/HitManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitManager : MonoBehaviour {
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	public void hoge(){
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Enemy")
+		{
+			//ゲームオーバー処理
+
+			//操作フラグの切り替え
+
+			//UI表示
+
+			//スタート画面準備
+		}
+	}
+}
