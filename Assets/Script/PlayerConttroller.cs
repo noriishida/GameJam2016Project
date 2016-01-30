25b70848206bac62dc5a65c49a3eb451e63d4f00
norioishidda <aoiaoitikyuu@gmail.com>
2016-01-30 22:55:30 +0900

    hoge

--- Assets/Script/PlayerConttroller.cs
+++ Assets/Script/PlayerConttroller.cs
@@ -7,13 +7,10 @@ public class PlayerConttroller : MonoBehaviour {
 	private float timer;
 
 	private float jumpPower = -4.0f;
-	private float ;
-
-	private Animator anim;
 
 	void Start()
 	{
-		anim = mouse.GetComponent<Animator> ();
+		
 	}
 
 	void Update () {
@@ -24,15 +21,15 @@ public class PlayerConttroller : MonoBehaviour {
 			var inputJump = Input.GetButton("Jump");
 			gameObject.transform.Rotate (0, inputX * timer *10, 0);
 
-			if(inputJump)
-			{
-				float posZ = mouse.transform.position.z;
-				mouse.transform.position.z += jumpPower * timer;
-				if(mouse.transform.position.z > posZ + jumpPower)
-				{
-					mouse.transform.position.z += jumpPower * timer;
-				}
-			}
+//			if(inputJump)
+//			{
+//				float posZ = mouse.transform.position.z;
+//				mouse.transform.position.z += jumpPower * timer;
+//				if(mouse.transform.position.z > posZ + jumpPower)
+//				{
+//					mouse.transform.position.z += jumpPower * timer;
+//				}
+//			}
 		}	
 	}
 }
