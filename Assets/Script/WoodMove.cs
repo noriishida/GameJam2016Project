203d904c29b4e99b7928a1ae417a49793267bcf6
norioishidda <aoiaoitikyuu@gmail.com>
2016-01-30 15:23:22 +0900

    ステージの生成。移動処理実装。

--- Assets/Script/WoodMove.cs
+++ Assets/Script/WoodMove.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WoodMove : MonoBehaviour {
-
-	private float speed = 3;
+public class WoodMove : MonoBehaviour 
+{
+	public float speed;
 	private float timer;
+	public Transform wood03;
+	private float instancePosY;
+	public Transform destroyPos;
 
-	void Start () {
-	
+	private InstanceObject instanceObject;
+
+	void Start () 
+	{
+		instancePosY = wood03.position.y;
+		instanceObject = FindObjectOfType<InstanceObject>();
 	}
 	
-	void Update () {
-		timer += Time.deltaTime;
-		gameObject.transform.position.y -= speed * timer;
+	void Update () 
+	{
+		if (PlayFlagManager.isPlaying)
+		{
+			timer = Time.deltaTime;
+			gameObject.transform.localPosition -= new Vector3 (0, speed * timer, 0);
+			if (gameObject.transform.position.y <= destroyPos.position.y)
+			{
+				Destroy (gameObject);
+				instanceObject.InstanceWoods(instancePosY);
+			}
+		}
 	}
 }
