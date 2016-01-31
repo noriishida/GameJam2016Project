91fcae3836bad2272c8d8bfcebd107d09248acf6
supersilph <supersilpheed@gmail.com>
2016-01-31 14:02:15 +0900

    ネズミ腰ふり

---
+++ Assets/Script/nezumiUgoki.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nezumiUgoki : MonoBehaviour {
+
+	public float furifuriSpeed;
+	public float furifuriPower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float furiAngle = Mathf.Sin( Time.time * this.furifuriSpeed ) * this.furifuriPower;
+		this.transform.localEulerAngles = new Vector3( 0, furiAngle, 0 );
+	
+	}
+}
