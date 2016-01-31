fabfab5e3905e9c69f3899f9c60e07b91122af0f
norioishidda <aoiaoitikyuu@gmail.com>
2016-01-31 11:46:56 +0900

    SE・BGM実装

--- Assets/Script/HitManager.cs
+++ Assets/Script/HitManager.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class HitManager : MonoBehaviour {
 
 	private PlayFlagManager playFlagManager;
+	private SoundManager soundManager;
 
 	void Start () 
 	{
 		playFlagManager = FindObjectOfType<PlayFlagManager> ();
+		soundManager = FindObjectOfType<SoundManager> ();
 	}
 	
 	void Update () 
@@ -24,16 +26,16 @@ public class HitManager : MonoBehaviour {
 	{
 		if (collider.gameObject.tag == "Enemy")
         {
-
+			soundManager.PlaySE (2);
             //ゲームオーバー処理
             Destroy(gameObject);
             //操作フラグの切り替え
             playFlagManager.isPlaying = false;
             //UI表示
             playFlagManager.isGameOver = true;
-
+			soundManager.PlaySE (1);
             //スタート画面準備
-            if (Input.GetKey(KeyCode.Space))
+			if (Input.GetButton("Jump"))
             {
                 int levelIndex = Application.loadedLevel;
             }
